162de0dfe08e99350ba3d498eb7ad184a3e19b58
fans <fan.jinlong@qq.com>
2019-08-12 17:14:53 +0800

    update

--- src/controller/index.go
+++ src/controller/index.go
@@ -5,14 +5,14 @@ package controller
 import (
 	"context"
 	"fmt"
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/mvc"
-	"github.com/robfig/cron/v3"
-	"google.golang.org/grpc"
 	"github.com/jinyuyoulong/jdcrontab/src/bootstrap/service"
 	"github.com/jinyuyoulong/jdcrontab/src/library/crontab"
 	"github.com/jinyuyoulong/jdcrontab/src/library/rpc"
 	"github.com/jinyuyoulong/jdcrontab/src/models"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"github.com/robfig/cron/v3"
+	"google.golang.org/grpc"
 	"log"
 	"os"
 	"strconv"
@@ -40,21 +40,21 @@ func (c *IndexController) Get() mvc.Result {
 	}
 }
 
-func (c *IndexController)GetInfo() mvc.Result  {
+func (c *IndexController) GetInfo() mvc.Result {
 	return mvc.View{
-		Name:"index/info.html",
-		Data:iris.Map{
-			"Title": commonTitle,
-			"Data": "",
+		Name: "index/info.html",
+		Data: iris.Map{
+			"Title":    commonTitle,
+			"Data":     "",
 			"AppOwner": service.AppConfig().App.AppOwner,
 		},
 	}
 }
 
 // url: /dojob
-func (c *IndexController)GetDojob()  {
+func (c *IndexController) GetDojob() {
 	jobId, _ := c.Ctx.URLParamInt("jobId")
-	c.Ctx.Writef("jobId = %v\n",jobId)
+	c.Ctx.Writef("jobId = %v\n", jobId)
 	jobModel := models.JdcronTasks{}.GetBy(jobId)
 
 	if jobModel.Id != 0 {
@@ -64,23 +64,23 @@ func (c *IndexController)GetDojob()  {
 
 		if jobModel.Command != "" {
 
-			go models.DoJob(jobModel.Command,jobModel.Handler)
-		}else {
+			go models.DoJob(jobModel.Command, jobModel.Handler)
+		} else {
 			log.Println("command 为空！")
 		}
-	}else {
+	} else {
 		c.Ctx.Writef("taskID 参数值错误")
 	}
 }
 
-func startJob(task *models.JdcronTasks)  {
+func startJob(task *models.JdcronTasks) {
 
-	log.Printf("start job name \"%v\"\n",task.Name)
+	log.Printf("start job name \"%v\"\n", task.Name)
 	spec := task.Spec
 	entryID := crontab.CycleJob(spec, task)
-	log.Println("entry id is ",entryID)
+	log.Println("entry id is ", entryID)
 
-	models.JdcronTasks{}.UpdateTID(task.Id, strconv.FormatInt(int64(entryID),10),1)
+	models.JdcronTasks{}.UpdateTID(task.Id, strconv.FormatInt(int64(entryID), 10), 1)
 
 }
 
@@ -89,7 +89,7 @@ func (c *IndexController) GetStartjobs() {
 	//wg := sync.WaitGroup{}
 	//wg.Add(1)
 
-	for _,task := range tasks{
+	for _, task := range tasks {
 
 		ttask := task
 		// 开启任务
@@ -103,23 +103,22 @@ func (c *IndexController) GetStartjobs() {
 	c.Ctx.Writef("start jobs")
 }
 
-
 func (c *IndexController) GetRemovejob() {
 	tid, err := c.Ctx.URLParamInt("taskID")
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 	log.Println("RemovejobBy")
 	removeJob(tid)
-	c.Ctx.Writef("终止 task id=",tid)
+	c.Ctx.Writef("终止 task id=", tid)
 }
 
-func removeJob(taskID int)  {
+func removeJob(taskID int) {
 	crontab.RemoveJob(cron.EntryID(taskID))
-	models.JdcronTasks{}.UpdateTaskStatus(taskID,0)
+	models.JdcronTasks{}.UpdateTaskStatus(taskID, 0)
 }
 
-func (c *IndexController)GetRemovealljobs()  {
+func (c *IndexController) GetRemovealljobs() {
 	tasks := models.JdcronTasks{}.GetAll()
 	for _, task := range tasks {
 		ttask := task
@@ -131,30 +130,30 @@ func (c *IndexController)GetRemovealljobs()  {
 }
 
 // cron test job
-func (c *IndexController) GetGetlog() *APIJson  {
-	return APIResult(0,"haha","")
+func (c *IndexController) GetGetlog() *APIJson {
+	return APIResult(0, "haha", "")
 }
 
 // RPC——
 // /rpc
 // rpc 通过 http 调用启动，或通过tcp调用响应（暂时没有头绪）
-func (c *IndexController)GetRpc() {
+func (c *IndexController) GetRpc() {
 	command := c.Ctx.URLParam("command")
 	if command == "" {
 		command = "localhost:8000"
 	}
-	log.Printf("rpc command is :%v\n",command)
+	log.Printf("rpc command is :%v\n", command)
 	//c.Ctx.Writef("%v",APIResult(0,fmt.Sprintf("GetRpc command is : ",command),"nil"))
 
-	connectRPC(c.Ctx,command)
+	connectRPC(c.Ctx, command)
 
 }
 
-func connectRPC(ictx iris.Context, command string)  {
+func connectRPC(ictx iris.Context, command string) {
 	//address := "http://0.0.0.0:8100"
 	defaultName := "world!"
 	// 建立连接
-	conn, err := grpc.Dial(command,grpc.WithInsecure())
+	conn, err := grpc.Dial(command, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal("grpc connction %v", err)
 		return
@@ -171,25 +170,24 @@ func connectRPC(ictx iris.Context, command string)  {
 	defer cancel()
 
 	// 获取 rpc 返回的数据
-	r, err := c.GRPCResponse(ctx, &rpc.GRPCRequest{Name:name})
+	r, err := c.GRPCResponse(ctx, &rpc.GRPCRequest{Name: name})
 	if err != nil {
 		//log.Fatal("can not get :%v", err)
-		log.Printf("connectRPC can not get %v\n",err)
+		log.Printf("connectRPC can not get %v\n", err)
 		return
 	}
-	log.Printf("greeting code:%v msg:%v data:%v\n",r.Code, r.Message,r.Data)
+	log.Printf("greeting code:%v msg:%v data:%v\n", r.Code, r.Message, r.Data)
 
-	ictx.Writef("greeting code:%v msg:%v data:%v\n",r.Code, r.Message,r.Data)
+	ictx.Writef("greeting code:%v msg:%v data:%v\n", r.Code, r.Message, r.Data)
 
 }
 
-func (c *IndexController)GetAdd() mvc.Result {
+func (c *IndexController) GetAdd() mvc.Result {
 	return mvc.View{
 		Name: "index/add.html",
-		Data:iris.Map{
+		Data: iris.Map{
 			"Title": "添加job",
 		},
-
 	}
 }
 func (c *IndexController) PostAdd() {
@@ -200,12 +198,12 @@ func (c *IndexController) PostAdd() {
 	spec := c.Ctx.PostValueTrim("spec")
 	command := c.Ctx.PostValue("command")
 	handler := c.Ctx.PostValue("handler")
-	addJob(name,spec,command,handler)
+	addJob(name, spec, command, handler)
 	json, err := c.Ctx.JSON(APIResult(0, "添加成功!", ""))
-	if err != nil{
-		log.Printf("post add json result:\n%v \nerr:\n%v",json,err)
+	if err != nil {
+		log.Printf("post add json result:\n%v \nerr:\n%v", json, err)
 	}
 }
-func addJob(name,spec,cmd,handler string)  {
-	models.JdcronTasks{}.Add(name,spec,cmd,handler)
+func addJob(name, spec, cmd, handler string) {
+	models.JdcronTasks{}.Add(name, spec, cmd, handler)
 }
--- src/models/jdcron_tasks.go
+++ src/models/jdcron_tasks.go
@@ -7,11 +7,9 @@ import (
 type JdcronTasks struct {
 	Id         int       `xorm:"not null pk INT(11)"`
 	TaskId     int       `xorm:"not null INT(11)"`
-	Name   	   string    `xorm:"VARCHAR(255)"`
+	Name       string    `xorm:"VARCHAR(255)"`
 	Spec       string    `xorm:"VARCHAR(255)"`
 	Command    string    `xorm:"VARCHAR(255)"`
 	Status     int       `xorm:"not null default 0 TINYINT(1)"`
-	Handler string
 	CreateTime time.Time `xorm:"not null DATETIME"`
 }
-
